ab616b49751e891cdf84830d953d20a0200d2da2
royyandzakiy <royyan.dzakiy@efishery.com>
2021-09-10 16:01:54 +0700

    feat(pumphandler):added configs

--- src/PumpHandler.cpp
+++ src/PumpHandler.cpp
@@ -1,15 +1,33 @@
 #include "PumpHandler.h"
 
-PumpHandler pumpHandler;
+PumpHandler pumpIn;
+PumpHandler pumpOut;
+
+#define PWM_BIT_RESOLUTION 8 // change this to the desired resolution
+
+#if(PWM_BIT_RESOLUTION == 8)
+#define PWM_MAX_BIT_VALUE 255
+#elif(PWM_BIT_RESOLUTION == 9)
+#define PWM_MAX_BIT_VALUE 511
+#elif(PWM_BIT_RESOLUTION == 10)
+#define PWM_MAX_BIT_VALUE 1023
+#endif
 
 // PWM Properties
 const int freq = 5000;
 const int pumpChannel = 0;
-const int resolution = 8;
+const int resolution = PWM_BIT_RESOLUTION;
 
-void PumpHandler::init() {
+void PumpHandler::init(const uint8_t pumpPin) {
     ledcSetup(pumpChannel, freq, resolution);
-    ledcAttachPin(PUMP_RELAY_PIN, pumpChannel);
+    ledcAttachPin(pumpPin, pumpChannel);
+
+    state = false;
+
+    kp = 0.1;
+    ki = 1.0;
+    error = 0.0;
+    errorSum = 0.0;
 }
 
 unsigned int PumpHandler::get() {
@@ -37,19 +55,75 @@ void PumpHandler::poll() {
     ledcWrite(pumpChannel, dutyCycle);
 }
 
+bool PumpHandler::isActive() {
+    return state;
+}
+
 void ledTest() {
-    for (int dutyCycle = 0; dutyCycle < 256; dutyCycle++) {
-        pumpHandler.set(dutyCycle);
+    for (int dutyCycle = 0; dutyCycle <= PWM_MAX_BIT_VALUE; dutyCycle++) {
+        pumpIn.set(dutyCycle);
         delay(15);
     }
-    for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-        pumpHandler.set(dutyCycle);
+    for (int dutyCycle = PWM_MAX_BIT_VALUE; dutyCycle >= 0; dutyCycle--) {
+        pumpIn.set(dutyCycle);
         delay(15);
     }
 }
 
 void PumpHandler::meassure(float desiredDebit) {
-    float error = 0.0;
-    error = desiredDebit-flowSensor.get();
-    // do other things
+    error = desiredDebit - florSensorOut.get();
+    errorSum+=error;
+    
+    float pwmVal = kp * error + ki * errorSum;
+    if (pwmVal > PWM_MAX_BIT_VALUE) {
+        pwmVal = PWM_MAX_BIT_VALUE;
+    } else if (pwmVal < 0) {
+        pwmVal = 0;
+    }
+    if (errorSum > 1000) {
+        errorSum = 1000;
+    } else if (errorSum < -1000) {
+        errorSum = -1000;
+    }
+    
+    set(PWM_MAX_BIT_VALUE - (int)pwmVal);
+}
+
+float PumpHandler::get(const char* _key) {
+    if (strcmp(_key, "ki") == 0) {
+        return kp;
+    } else if (strcmp(_key, "kp") == 0) {
+        return ki;
+    } else if (strcmp(_key, "debitTarget") == 0) {
+        return debitTarget;
+    } else {
+        _PTF("    | ERROR: \"%s\" not found.\n", _key);
+    }
+}
+
+void PumpHandler::set(const char* _key, const char* _value) {
+    if (strcmp(_key, "ki") == 0) {
+        kp = atof(_value);
+    } else if (strcmp(_key, "kp") == 0) {
+        ki = atof(_value);
+    } else if (strcmp(_key, "debitTarget") == 0) {
+        debitTarget = atof(_value);
+    } else if (strcmp(_key, "start") == 0) {
+        start();
+    } else if (strcmp(_key, "stop") == 0) {
+        stop();
+    } else {
+        _PTF("    | ERROR: \"%s\" not found.\n", _key);
+    }
+}
+
+void PumpHandler::printConfiguration() {
+    _PTF("    | ------ PUMP CONFIGURATION ------\n");
+    _PTF("    | \n");
+    _PTF("    |     ki: %f\n", get("ki"));
+    _PTF("    |     kp: %f\n", get("kp"));
+    _PTF("    |     debitTarget: %f\n", get("debitTarget"));
+    _PTF("    |     state: %s\n", state ? "running" : "stopped");
+    _PTF("    | \n");
+    _PTF("    | ---------------------------\n");
 }
--- src/PumpHandler.h
+++ src/PumpHandler.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <Arduino.h>
+#include "Utilities.h"
 #include "PinDefinitions.h"
 #include "FlowSensor.h"
 
 class PumpHandler {
     public:
-        void init();
+        void init(const uint8_t);
         void set(const unsigned int);
         unsigned int get();
         
@@ -13,10 +14,23 @@ class PumpHandler {
         void stop();
         void poll();
         void meassure(float);
+        bool isActive();
+
+        float get(const char *);
+        void set(const char *, const char *);
+        void printConfiguration();
 
     private:
         unsigned int dutyCycle;
         bool state;
+
+        float kp;
+        float ki;
+        float debitTarget;
+        
+        float error;
+        float errorSum;
 };
 
-extern PumpHandler pumpHandler;
+extern PumpHandler pumpIn;
+extern PumpHandler pumpOut;
